09aecbc0252c0a45814a51fa71feeb13174707b2
Himashi Hettege Dona <himashi99@hotmail.com>
2018-08-09 19:48:45 -0400

    Add countdown clock

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -8,3 +8,61 @@ $(document).ready(function(){
     }
   });
 });
+
+// Countdown Timer
+
+function countDown() {
+  let now = new Date();
+  let eventDate = new Date(2018, 9, 13);
+
+  let currentTime = now.getTime();
+  let eventTime = eventDate.getTime();
+
+  // Time until the event in ms
+  let remTime = eventTime - currentTime;
+
+  // converts ms to seconds
+  // math.floor rounds number down to nearest whole integer
+  let s = Math.floor(remTime / 1000);
+
+  let m = Math.floor(s / 60);
+
+  let h = Math.floor(m / 60);
+
+  let d = Math.floor(h / 24);
+
+  //remaining hours, minutes and seconds
+  h %= 24;
+  m %= 60;
+  s %= 60;
+
+
+  if (h < 10) {
+    h = "0" + h;
+  } else {
+    h = h;
+  }
+
+  if (m < 10) {
+    m = "0" + m;
+  } else {
+    m = m;
+  }
+
+  if (s < 10) {
+    s = "0" + s;
+  }   else {
+    s = s;
+  } 
+
+
+  document.getElementById("days").innerHTML = d;
+  document.getElementById("hours").innerHTML = h;
+  document.getElementById("minutes").innerHTML = m;
+  document.getElementById("seconds").innerHTML = s;
+
+  setTimeout(countDown, 1000);
+
+}
+
+countDown();
