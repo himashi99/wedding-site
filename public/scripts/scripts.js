f9daf19c879a7a6e171ea965a9b1b7a2ab8a6deb
Himashi Hettege Dona <himashi99@hotmail.com>
2018-08-13 18:39:32 -0400

    Finish script for faq section

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -80,17 +80,147 @@ countDown();
 
 // FAQ Show Answers
 
-document.getElementById("arrow-down").onclick = function() {
-  var elements = document.getElementById("test");
+// grabs all divs with class name "arrow" and assigns it to variable arrowAction
+// let arrowAction = document.getElementsByClassName("arrow");
+// let answersLoop = document.getElementsByClassName("question-answer")
 
-    if (elements.classList.contains("question-answer-hide")) {
+// // loops over each div with class name "arrow"
+// for(var i = 0; i < arrowAction.length; i++) {
+//   for(var j=0; j < answersLoop.length; j++) {
+//     arrowAction[i].onclick = function() {
+//       if(answersLoop[j].classList.contains("questions-answer-hide")) {
+//         answersLoop[j].classList.add("question-answer-show");
+//         answersLoop[j].this.classList.remove("question-answer-hide");
+//       } else {
+//         answersLoop[j].classList.add("question-answer-hide");
+//         answersLoop[j].classList.remove("question-answer-show");
+//       }
+//     }
+//   }
+// }
 
-      console.log("whooho")
-      elements.classList.add("question-answer-show");
-      elements.classList.remove("question-answer-hide");
+///////////////////////////////////////////////////////////////////
 
-    } else {
-      elements.classList.add("question-answer-hide");
-      elements.classList.remove("question-answer-show");
-    }
+let arrowButton = document.getElementsByClassName("arrow");
+let answers = document.getElementsByClassName("question-answer");
+
+for(let i = 0; i < arrowButton.length; i ++) {
+    arrowButton[i].addEventListener("click", function() {
+        arrowButton[i].id = "arrowID_" + i;
+        answers[i].id = "answersID_" + i;
+
+        answers[i].setAttribute('id','answersID_'+i);
+        console.log('answersID_'+i);
+  
+        showHide();
+    })
+  
+
+
+
+
+function showHide() {
+    let answersElement = document.getElementById('answersID_' +i)
+        // console.log(answers[i].id)
+
+        if(answersElement.classList.contains("question-answer-hide")) {
+          // console.log("whooho")
+          answersElement.classList.add("question-answer-show");
+          answersElement.classList.remove("question-answer-hide");
+        } else {
+          answersElement.classList.add("question-answer-hide");
+          answersElement.classList.remove("question-answer-show");    
+        }
 }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////// PRASANTH
+// let answers = document.getElementsByClassName("question-answer")
+// let buttons = document.getElementsByClassName("arrow");
+
+// for(let i = 0; i < answers.length; i++) {
+//   // create ids
+//   let answerId = 'answer-' + i;
+//   let buttonId = 'answer-btn' + i;
+//   // set id
+//   answers[i].setAttribute('id', answerId);
+//   // for each answer attach a click listener
+//   buttons[i].addEventListener('click', function() {
+//     const ele = document.getElementById(answerId);
+//     showHide(ele);
+//   });
+// }
+
+// function showHide(ele) {
+//   if(ele.classList.contains("question-answer-hide")) {
+//     ele.classList.add("question-answer-show");
+//     ele.classList.remove("question-answer-hide");
+//   } else {
+//     ele.classList.add("question-answer-hide");
+//     ele.classList.remove("question-answer-show");
+//   }
+// }
+//////////////////////////////////////////////////
+
+
+// let arrowAction = document.getElementsByClassName("arrow");
+// let answersLoop = document.getElementsByClassName("question-answer")
+
+// for(var i = 0; i < arrowAction.length; i++) {
+  
+//   arrowAction[i].onclick = function() {
+//     console.log("Hi");
+//     var elements = document.getElementById("test");
+
+//     if (elements.classList.contains("question-answer-hide")) {
+
+//       console.log("whooho")
+//       elements.classList.add("question-answer-show");
+//       elements.classList.remove("question-answer-hide");
+
+//     } else {
+//       elements.classList.add("question-answer-hide");
+//       elements.classList.remove("question-answer-show");
+//     }
+//   }
+// }
+
+
+// document.getElementById("arrow-down").onclick = function() {
+//   var elements = document.getElementById("test");
+
+//     if (elements.classList.contains("question-answer-hide")) {
+
+//       console.log("whooho")
+//       elements.classList.add("question-answer-show");
+//       elements.classList.remove("question-answer-hide");
+
+//     } else {
+//       elements.classList.add("question-answer-hide");
+//       elements.classList.remove("question-answer-show");
+//     }
+// }
